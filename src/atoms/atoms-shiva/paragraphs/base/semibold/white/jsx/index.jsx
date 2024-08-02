8a5a41401aa62af5633e15cadbf193f68d0c3ef9
Shiva pandey <shivapandey9898@gmail.com>
2024-08-02 12:21:32 +0530

    feat: created paragraph base white jsx atom

---
+++ src/atoms/atoms-shiva/paragraphs/base/semibold/white/jsx/index.jsx
@@ -0,0 +1,12 @@
+const ParagraphBaseSemiboldWhite = ( props ) => {
+
+    return (
+
+        <p className="text-white font-poppins font-semibold">
+            { props.children }
+        </p>
+
+    )
+
+}
+export default ParagraphBaseSemiboldWhite
